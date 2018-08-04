06e72875efb09cd5a4449da657af553aac540c3f
jpramirez <jpramirez@localhost>
2018-08-03 20:15:50 -0500

    TG-1 #close

--- src/main/java/com/mycompany/test/TestCI.java
+++ src/main/java/com/mycompany/test/TestCI.java
@@ -17,7 +17,8 @@ public class TestCI {
     }
     //hiiiii
     int product(int n1, int n2){
-        return n1 * n2;
+        return n1 * n2 * 2;
+        
     }
     
     public static void main(String[] args) {
