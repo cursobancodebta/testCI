4b9562b884b2cc07a5baf3fc9ed2b67cbd66b130
jpramirez <jpramirez@localhost>
2018-08-03 20:32:30 -0500

    TG-1 #in progress

--- src/main/java/com/mycompany/test/TestCI.java
+++ src/main/java/com/mycompany/test/TestCI.java
@@ -15,7 +15,7 @@ public class TestCI {
     public int sum(int n1, int n2){
         return n1 + n2;
     }
-    //hiiiiiyyyyyyyuuuu
+    //hiiiiiyyyyyyyuuuusadasdasd
     int product(int n1, int n2){
         return n1 * n2 * 2;
         
